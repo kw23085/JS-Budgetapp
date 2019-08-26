8b50383bab025458b95fa362d20db70f4d6adb0d
Kevin Wang <kw1017@gmail.com>
2019-08-26 14:00:31 -0700

    button

--- app.js
+++ app.js
@@ -8,12 +8,20 @@ var budgetController = (function() {
 
 // UI CONTROLLER
 var UIController = (function() {
+
+    var DOMstrings = {
+        inputType: ".add__type",
+        inputDescription: ".add__description",
+        inputValue: ".add__value"
+    };
     
     return {
         getinput: function() {
-            var type = document.querySelector('.add__type').value; // Will be either inc or exp
-            var description = document.querySelector('.add__description').value;
-            var value = decument.querySelector('.add__value').value;
+            return {
+                type: document.querySelector(DOMstrings.inputType).value, // Will be either inc or exp
+                description: document.querySelector(DOMstrings.inputDescription).value,
+                value: document.querySelector(DOMstrings.inputValue).value
+            };
         }
     };
 
@@ -26,6 +34,8 @@ var controller = (function(budgetCtrl, UICtrl) {
     var ctrlAddItem = function() {
 
             // 1. Get the filed input data
+            var input = UICtrl.getinput();
+            console.log(input);
 
             // 2. Add the item to the budget controller
 
@@ -34,8 +44,6 @@ var controller = (function(budgetCtrl, UICtrl) {
             // 4. Calculate the budget
 
             // 5. Display the budgeton the UI
-
-            console.log('It works.')
 
     }
 
