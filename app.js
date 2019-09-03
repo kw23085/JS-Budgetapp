04ec809c0b66ef21dc96f109b030b4fc059bd29d
Kevin Wang <kw1017@gmail.com>
2019-09-03 15:03:42 -0700

    delete UI

--- app.js
+++ app.js
@@ -166,6 +166,12 @@ var UIController = (function() {
 
         },
 
+        deleteListItem: function(selectorID) {
+
+            var el = document.getElementById(selectorID);
+            el.parentNode.removeChild(el);
+        },
+
         clearFields: function() {
             var fields, fieldsArr;
 
@@ -275,8 +281,10 @@ var controller = (function(budgetCtrl, UICtrl) {
             budgetCtrl.deleteItem(type, ID);
 
             // 2. Delete the item from the UI
+            UIController.deleteListItem(itemID);
 
             // 3. Update and show the new budget
+            updateBudget();
         }
 
     }
